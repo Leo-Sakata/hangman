e3cdc24f9da47be017bf549b4005a2e9cff30a6d
Leo SAKATA <s1F101802803@iniad.org>
2022-02-19 11:01:59 +0900

    my first commit

---
+++ 10.py
@@ -0,0 +1,49 @@
+import random
+
+words=["togashi", 'ishida', 'nakamura', 'suzuki', 'hara', 'nagasaka', 'sasaki', 'miki', 'nishimura', 'fujiwara', 'ogino', 'yonekura']
+
+def hangman(word):
+  wrong=0
+  stages=["",
+          "__________        ",
+          "|                 ",
+          "|         |       ",
+          "|         O       ",
+          r"|        /|\      ",
+          r"|        / \      ",
+          "|                 "
+          ]
+  rletters=list(word)
+  board=["_"]*len(word)
+  win=False
+  print('ハングマンへようこそ！')
+
+  while wrong<len(stages)-1:
+    print('\n')
+    msg='1文字予想してね'
+    char=input(msg)
+
+    if char in rletters:
+      cind=rletters.index(char)
+      board[cind]=char
+      rletters[cind]='$'
+
+    else:
+      wrong+=1
+      
+    print(" ".join(board))
+    e=wrong+1
+    print("\n".join(stages[0:e]))
+
+    if '_' not in board:
+      print('あなたの勝ち！')
+      win=True
+      break
+
+  if not win:
+    print('\n'.join(stages[0:wrong+1]))
+    print("あなたの負け！正解は{}".format(word))
+
+n=random.randrange(len(words))
+
+hangman(words[n])
